4cdfcecddba3de8fb6714a323c636d3315d67ea8
satwikag-cmd <satwika.g@kasplo.com>
2026-06-23 11:24:45 +0530

    Modified utils/env.ts

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,7 +1,3 @@
 export const env = {
-  // 💻 Hardcoding his exact laptop IP address right here:
-  NEXT_PUBLIC_API_URL: 'http://192.168.88.25:8081',
-
-  // 🔑 Hardcoding his exact case-sensitive security key right here:
-  // NEXT_PUBLIC_API_KEY: 'a6b33986d7afd06bcdd55fbd22590c7121ab3b3e3e4c8218226dba77d7c5ae0d',
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://192.168.88.31:8081',
 } as const;
